dc863495f8db168f7777b2b952e7d5ecbf2e2c1a
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2021-10-30 20:51:55 +0700

    new wallet mechanism

--- packages/cosmos/src/bech32/bech32.spec.ts
+++ packages/cosmos/src/bech32/bech32.spec.ts
@@ -1,15 +1,11 @@
-import {PrivKeyEthSecp256k1, Mnemonic} from "@keplr-wallet/crypto";
-import {Bech32Address} from "./index";
-import {Crypto} from "@keplr-wallet/background/src/keyring/crypto";
-import { ScryptParams } from "@keplr-wallet/background"
+import {PrivKeyEthSecp256k1} from "@keplr-wallet/crypto";
+//import {Bech32Address} from "./index";
 
 import assert from "assert";
-import scrypt from "scrypt-js"
 
 // import {Bech32Address} from "./index";
 //import EC from "elliptic";
-import * as util from 'ethereumjs-util';
-//import Wallet from 'ethereumjs-wallet';
+import {Wallet} from 'ethers';
 
 const MNEUMONIC = "visit drastic version push field lake suit bulb tilt private erode describe"
 const ETH_ADDRESS_FROM_MNEUMONIC = "0x471C5E8e694AfB1b2212428FD760265A28124cb0";
@@ -18,67 +14,21 @@ const ETH_ADDRESS_FROM_PRIV = "0x6cF77c4EaA3f9A2449643D5Efa1D0C43583459F2";
 
 describe('simulating construct private key from mnemonic', () => {
     it('should generate correct eth address', async () => {
-        // simulate creating and storing mnemonic
-        const res = await Crypto.encrypt(
-            (array) => {
-                return Promise.resolve(crypto.getRandomValues(array));
-            },
-            {
-                scrypt: async (text: string, params: ScryptParams) => {
-                  return await scrypt.scrypt(
-                    Buffer.from(text),
-                    Buffer.from(params.salt, "hex"),
-                    params.n,
-                    params.r,
-                    params.p,
-                    params.dklen
-                  );
-                },
-            },
-            "sha256",
-            "mnemonic",
-            trimWordsStr(MNEUMONIC),
-            "",
-            {"":""},
-            undefined
-        )
-        
-        // simulate retrieving mnemonic
-        const mnemonic = Buffer.from(await Crypto.decrypt(
-            {
-                scrypt: async (text: string, params: ScryptParams) => {
-                  return await scrypt.scrypt(
-                    Buffer.from(text),
-                    Buffer.from(params.salt, "hex"),
-                    params.n,
-                    params.r,
-                    params.p,
-                    params.dklen
-                  );
-                },
-            },
-            res,
-            ""
-        )).toString();
-
         //simulate getting private key
-        const privKeyBuffer = Mnemonic.generateWalletFromMnemonic(mnemonic);
+        const wallet = Wallet.fromMnemonic(trimWordsStr(MNEUMONIC));
 
-        const privKey = new PrivKeyEthSecp256k1(privKeyBuffer);
-
-        const pubKey = privKey.getPubKey();
-        console.log(pubKey.getAddress().length);
-        const address = new Bech32Address(pubKey.getAddress());
-        console.log(address);
-        const ethAddress = address.toBech32("");
-        console.log(ethAddress)
-        assert.strictEqual(ethAddress, ETH_ADDRESS_FROM_MNEUMONIC);
+        const privKeyEth = new PrivKeyEthSecp256k1(wallet);
+        const pubKey = privKeyEth.getPubKey()
+        const addressBuffer = pubKey.getAddress();
+        const publicAddress = pubKey.getAddressString();
+        console.log(addressBuffer)
+        assert.strictEqual(publicAddress, ETH_ADDRESS_FROM_MNEUMONIC);
     })
 })
 
 describe("Test eth derivation", () => {
     it("priv key should generate the eth address", () => {
-        const privateKeyEth = new PrivKeyEthSecp256k1(util.toBuffer(util.addHexPrefix(PRIV_KEY)))
+        const privateKeyEth = new PrivKeyEthSecp256k1(new Wallet(PRIV_KEY));
         const pubKey = privateKeyEth.getPubKey()
         const addressBuffer = pubKey.getAddress();
         const publicAddress = pubKey.getAddressString();
