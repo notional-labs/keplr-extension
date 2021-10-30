0f2f77b23b061de0795029219df9721d1bb3d2c6
Brenn Hill <brennhill@gmail.com>
2021-10-30 13:52:59 +0700

    Add eth private/public classes based on unit test

--- packages/cosmos/src/bech32/bech32.spec.ts
+++ packages/cosmos/src/bech32/bech32.spec.ts
@@ -1,9 +1,8 @@
-//import {PrivKeyEthSecp256k1} from "@keplr-wallet/crypto";
+import {PrivKeyEthSecp256k1} from "@keplr-wallet/crypto";
 // import {Bech32Address} from "./index";
 import assert from "assert";
 //import EC from "elliptic";
 import * as util from 'ethereumjs-util';
-// import {publicKeyCreate} from "ethereum-cryptography/secp256k1";
 import Wallet from 'ethereumjs-wallet';
 //const MNEUMONIC = "celery husband drama unaware blue empower jelly twist program say prepare page"
 const PRIV_KEY = "0b0df1b5ee8e70227e26a1b954f1c20ab4c043f7b045421bf12baa043429ede6"
@@ -11,13 +10,11 @@ const ETH_ADDRESS_FROM_PRIV = "0x6cF77c4EaA3f9A2449643D5Efa1D0C43583459F2";
 
 describe("Test eth derivation", () => {
     it("priv key should generate the eth address", () => {
-
+        const privateKeyEth = new PrivKeyEthSecp256k1(util.toBuffer(util.addHexPrefix(PRIV_KEY)))
         const privateKeyBuffer = util.toBuffer(util.addHexPrefix(PRIV_KEY));
         const wallet = Wallet.fromPrivateKey(privateKeyBuffer);
         const publicAddress = wallet.getAddressString();
-        // const publicKey = wallet.getPublicKeyString();
-        //const publickey  = util.privateToPublic(privateKeyBuffer)
-        //const publicAddress = util.publicToAddress(publickey);
+
         assert.strictEqual(publicAddress, ETH_ADDRESS_FROM_PRIV);
     });
 });
