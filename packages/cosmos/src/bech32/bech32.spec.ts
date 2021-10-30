4777da75a2782f5e48e8409944ab9f92ec22bed8
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2021-10-30 17:26:46 +0700

    merge

--- packages/cosmos/src/bech32/bech32.spec.ts
+++ packages/cosmos/src/bech32/bech32.spec.ts
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import {PrivKeySecp256k1, Mnemonic} from "@keplr-wallet/crypto";
+import {PrivKeyEthSecp256k1, Mnemonic} from "@keplr-wallet/crypto";
 import {Bech32Address} from "./index";
 import {Crypto} from "@keplr-wallet/background/src/keyring/crypto";
 import { ScryptParams } from "@keplr-wallet/background"
@@ -7,17 +6,13 @@ import { ScryptParams } from "@keplr-wallet/background"
 import assert from "assert";
 import scrypt from "scrypt-js"
 
-const MNEUMONIC = "visit drastic version push field lake suit bulb tilt private erode describe"
-const ETH_ADDRESS_FROM_MNEUMONIC = "0x471C5E8e694AfB1b2212428FD760265A28124cb0";
-=======
-import {PrivKeyEthSecp256k1} from "@keplr-wallet/crypto";
 // import {Bech32Address} from "./index";
-import assert from "assert";
 //import EC from "elliptic";
 import * as util from 'ethereumjs-util';
 //import Wallet from 'ethereumjs-wallet';
-//const MNEUMONIC = "celery husband drama unaware blue empower jelly twist program say prepare page"
->>>>>>> 9c5ca9b90b15ba68a053c849b7045543caac8808
+
+const MNEUMONIC = "visit drastic version push field lake suit bulb tilt private erode describe"
+const ETH_ADDRESS_FROM_MNEUMONIC = "0x471C5E8e694AfB1b2212428FD760265A28124cb0";
 const PRIV_KEY = "0b0df1b5ee8e70227e26a1b954f1c20ab4c043f7b045421bf12baa043429ede6"
 const ETH_ADDRESS_FROM_PRIV = "0x6cF77c4EaA3f9A2449643D5Efa1D0C43583459F2";
 
@@ -69,7 +64,7 @@ describe('simulating construct private key from mnemonic', () => {
         //simulate getting private key
         const privKeyBuffer = Mnemonic.generateWalletFromMnemonic(mnemonic);
 
-        const privKey = new PrivKeySecp256k1(privKeyBuffer);
+        const privKey = new PrivKeyEthSecp256k1(privKeyBuffer);
 
         const pubKey = privKey.getPubKey();
         console.log(pubKey.getAddress().length);
@@ -83,24 +78,12 @@ describe('simulating construct private key from mnemonic', () => {
 
 describe("Test eth derivation", () => {
     it("priv key should generate the eth address", () => {
-<<<<<<< HEAD
-        const privKey = new PrivKeySecp256k1(Uint8Array.from(Buffer.from(PRIV_KEY, 'hex')));
-
-        const pubKey = privKey.getPubKey();
-        console.log(pubKey.getAddress().length);
-        const address = new Bech32Address(pubKey.getAddress());
-        console.log(address);
-        const ethAddress = address.toBech32("");
-        console.log(ethAddress)
-        assert.strictEqual(ethAddress, ETH_ADDRESS_FROM_PRIV);
-=======
         const privateKeyEth = new PrivKeyEthSecp256k1(util.toBuffer(util.addHexPrefix(PRIV_KEY)))
         const pubKey = privateKeyEth.getPubKey()
         const addressBuffer = pubKey.getAddress();
         const publicAddress = pubKey.getAddressString();
         console.log(addressBuffer)
         assert.strictEqual(publicAddress, ETH_ADDRESS_FROM_PRIV);
->>>>>>> 9c5ca9b90b15ba68a053c849b7045543caac8808
     });
 });
 
