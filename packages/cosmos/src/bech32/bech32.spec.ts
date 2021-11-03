24bcd466a2e419a1ed5765f2cbc7c1ad8c2d2fb3
vuong <nguyenvuong1122000@gmail.com>
2021-11-03 21:34:05 +0700

    change ethprivkey and pubkey method

---
+++ packages/cosmos/src/bech32/bech32.spec.ts
@@ -0,0 +1,20 @@
+import {PrivKeyEthSecp256k1} from "@keplr-wallet/crypto";
+// import {Bech32Address} from "./index";
+import assert from "assert";
+//import EC from "elliptic";
+import * as util from 'ethereumjs-util';
+//import Wallet from 'ethereumjs-wallet';
+//const MNEUMONIC = "celery husband drama unaware blue empower jelly twist program say prepare page"
+const PRIV_KEY = "0b0df1b5ee8e70227e26a1b954f1c20ab4c043f7b045421bf12baa043429ede6"
+const ETH_ADDRESS_FROM_PRIV = "0x6cF77c4EaA3f9A2449643D5Efa1D0C43583459F2";
+
+describe("Test eth derivation", () => {
+    it("priv key should generate the eth address", () => {
+        const privateKeyEth = new PrivKeyEthSecp256k1(util.toBuffer(util.addHexPrefix(PRIV_KEY)))
+        const pubKey = privateKeyEth.getPubKey()
+        const addressBuffer = pubKey.getAddress();
+        const addressHexStr = "0x" + Buffer.from(addressBuffer).toString('hex');
+        console.log(addressHexStr)
+        assert.strictEqual(addressHexStr, ETH_ADDRESS_FROM_PRIV);
+    });
+});
