344232dcfb5735ddbe146107a5a252f529f24811
vuong <nguyenvuong1122000@gmail.com>
2021-11-05 21:41:36 +0700

    add dig local chain in config

--- packages/extension/src/config.ts
+++ packages/extension/src/config.ts
@@ -1139,6 +1139,55 @@ export const EmbedChainInfos: ChainInfo[] = [
     },
     beta: true,
   },
+  {
+    rpc: "http://0.0.0.0:26657",
+    rpcConfig: BETA_STRAIGHTEDGE_RPC_CONFIG,
+    rest: "http://0.0.0.0:1317",
+    restConfig: BETA_STRAIGHTEDGE_REST_CONFIG,
+    chainId: "dig-1",
+    chainName: "DIG",
+    stakeCurrency: {
+      coinDenom: "stake",
+      coinMinimalDenom: "stake",
+      coinDecimals: 1,
+    },
+    walletUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://wallet.keplr.app/#/straightedge/stake"
+        : "http://localhost:8080/#/straightedge/stake",
+    walletUrlForStaking:
+      process.env.NODE_ENV === "production"
+        ? "https://wallet.keplr.app/#/straightedge/stake"
+        : "http://localhost:8080/#/straightedge/stake",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: Bech32Address.defaultBech32Config("str"),
+    currencies: [
+      {
+        coinDenom: "stake",
+        coinMinimalDenom: "stake",
+        coinDecimals: 1,
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "stake",
+        coinMinimalDenom: "stake",
+        coinDecimals: 1,
+      },
+    ],
+    coinType: 551,
+    // STR's decimal is high. Thus, if gas price is set as 0.025, it produces very low and long fee.
+    // And, currently, this long fee is not visible well in Keplr.
+    // Just, increase the gas price step temporarily.
+    gasPriceStep: {
+      low: 0.01 * Math.pow(10, 12),
+      average: 0.025 * Math.pow(10, 12),
+      high: 0.04 * Math.pow(10, 12),
+    },
+    beta: true,
+  },
 ];
 
 // The origins that are able to pass any permission that external webpages can have.
